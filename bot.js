8778f9ca41b5501bb2deafdacf4b83fe922929e2
FavoHD <hdfavo@gmail.com>
2020-04-05 16:32:30 +0200

    test

--- bot.js
+++ bot.js
@@ -62,7 +62,7 @@ function help(message, connection) {
 }
 
 function listUsers(message, connection) {
-	var string = "";
+	var string = "Userlist";
 
 	connection.query('SELECT * FROM `Favo_Eligored_users`', function (error, results, fields) {
 		results.forEach(user => {
